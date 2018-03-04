8f257760db5d6166fa38329c6378cf52f56e0bfe
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-03-04 22:42:47 +0200

    Exercise 3

--- week19-parsers/positive-parser/src/Main.hs
+++ week19-parsers/positive-parser/src/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
-import Safe (readMay)
-import Text.Trifecta
+import Control.Applicative
 import Data.Foldable (foldl')
+import Safe (readMay)
 import Text.Parser.Combinators
+import Text.Trifecta
 
+{- 2 -}
 parseDigit :: Parser Char
 parseDigit = oneOf "0123456789"
 
@@ -22,9 +24,18 @@ base10Integer = foldl' f 0 <$> (some parseDigit)
     f :: Integer -> Char -> Integer
     f acc c = acc * 10 + read [c]
 
+{- 3 -}
+base10Integer' :: Parser Integer
+base10Integer' =
+  try (fmap (* (-1)) (char '-' *> base10Integer)) <|> base10Integer
+
 main :: IO ()
-main = do
+main
+  {- 2 -}
+ = do
   print $ parseString parseDigit mempty "123"
   print $ parseString parseDigit mempty "abc"
   print $ parseString base10Integer mempty "123abc"
   print $ parseString base10Integer mempty "abc"
+  {- 3 -}
+  print $ parseString base10Integer' mempty "-123abc"
